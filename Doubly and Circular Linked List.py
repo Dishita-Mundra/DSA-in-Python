dcb9f95af5bab5d513c308b5dafe153cfb2bcf63
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-05-06 23:04:45 +0530

    Add files via upload

--- Doubly and Circular Linked List.py
+++ Doubly and Circular Linked List.py
@@ -1,4 +1,3 @@
-# yha tak pauch gyi haramkhor 
 class Node: # Node class for Doubly Linked List
     def __init__(self , value = None): # Constructor to initialize the node with data and pointers
         self.data = value # Data part of the node
@@ -31,6 +30,24 @@ class DoublyLL: # Doubly Linked List Class
         self.head.prev = temp # Set the previous pointer of the current head to the new node
         self.head = temp # Set the new node as the head of the list    
 
+    def insertAtMid(self,value,x): # Method to insert a node at a specific position in the list
+        t=self.head # Start from the head of the list
+
+        #SEARCHING CODE(upto line 41)
+        while(t.next != None): # Traverse the list until the end
+            if(t.data == x): # If the current node's data matches the given value x
+                break # Exit the loop to insert the new node at this position
+            else:
+                t = t.next # Move to the next node
+
+
+        temp=Node(value) # Create a new node with the given value
+        temp.next = t.next # Set the next pointer of the new node to the next node of the current node
+        t.next.prev = temp # Set the previous pointer of the next node to the new node
+        t.next = temp # Set the next pointer of the current node to the new node
+        temp.prev = t # Set the previous pointer of the new node to the current node
+
+
     def printDLL(self):
         t1=self.head # t1 is a temporary pointer to traverse the linked list
         
@@ -41,6 +58,27 @@ class DoublyLL: # Doubly Linked List Class
         
         print(t1.data) # print the data part of the last node
 
+    def deletionDLL(self,value): # Method to delete a node with a specific value from the list
+        if(self.head == None): # If the list is empty, print a message and return
+            print("Linked List is empty") # Print a message indicating that the list is empty
+            return # Exit the method 
+
+        t=self.head # Start from the head of the list
+
+        if(t.data == value): # If the head node's data matches the given value
+            self.head = t.next # Set the head to the next node
+            self.head.prev = None # Set the previous pointer of the new head to None
+
+        while(t.next != None): # Traverse the list until the end
+
+            if(t.data == value): # If the current node's data matches the given value
+                t.prev.next = t.next # Set the next pointer of the previous node to the next node
+                t.next.prev = t.prev # Set the previous pointer of the next node to the previous node
+                return # Exit the method after deleting the node
+            
+        if (t.data == value): # If the last node's data matches the given value
+            t.prev.next = None # Set the next pointer of the previous node to None
+            
 obj = DoublyLL() # Create an object of the DoublyLL class
 
 obj.insertAtEnd(10) # Insert a node with value 10 at the end of the list
@@ -52,5 +90,9 @@ obj.insertAtEnd(30) # Insert a node with value 30 at the end of the list
 obj.insertAtEnd(40) # Insert a node with value 40 at the end of the list
 
 obj.insertAtBeg(5) # Insert a node with value 5 at the beginning of the list
+
+obj.insertAtMid(50,20) # Insert a node with value 25 after the node with value 20
+
+obj.deletionDLL(5) # Delete the node with value 30 from the list
 
 obj.printDLL() # Print the elements of the list
