724273fe7610fbb1615c371ba4468b787a197c2e
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-05-08 22:12:00 +0530

    Add files via upload

--- Array.py
+++ Array.py
@@ -5,29 +5,96 @@ val=array('i',[1,2,3,4,5,6,7,8,9])
 # val=array('d',[1,2,3,4,5,6,7,8,9.5]) # double type array
 # val=array('u',['a','b','c','d','e']) # unicode character array
 
-print(val)
+# print(val)
 
-for i in range(0,6): # index 0 included and 6 excluded
-    print(val[i] )  # each element on a separate line
+# for i in range(0,6): # index 0 included and 6 excluded
+#     print(val[i] )  # each element on a separate line
 
-for i in range(0,6):
-     print(val[i] , end=" ") # all elements on the same line separated by a space
+# for i in range(0,6):
+#      print(val[i] , end=" ") # all elements on the same line separated by a space
 
-for i in range(0,len(val)): # elements from index 0 to the last index
-     print(val[i] , end=" ")
+# for i in range(0,len(val)): # elements from index 0 to the last index
+#      print(val[i] , end=" ")
 
-print('\n') # new line
-for x in val:
-     print(x , end=" , ") 
+# print('\n') # new line
+# for x in val:
+#      print(x , end=" , ") 
 
-print('\n')
-print(val.typecode) # type of array elements
+# print('\n')
+# print(val.typecode) # type of array elements
 
-val.reverse() 
-for i in range(0,len(val)): 
-     print(val[i] , end=" ")
+# val.reverse() 
+# for i in range(0,len(val)): 
+#      print(val[i] , end=" ")
 
-val.insert(1,50) # insert 50 at index 1
-val.append(100) # add 100 at the end of the array
-for i in range(0,len(val)):
-     print(val[i] , end=" ")
+# val.insert(1,50) # insert 50 at index 1
+# val.append(100) # add 100 at the end of the array
+# val[2]=200 # change the value at index 2 to 200
+# for i in range(0,len(val)):
+#      print(val[i] , end=" ")
+
+##COPY ARRAY
+# copyArray=array(val.typecode, (x for x in val)) # create a copy of the array
+# copyArray=array(val.typecode, (x*3 for x in val)) # create a copy of the array with each element multiplied by 3
+
+# copyArray.pop(3) # remove the element at index 3
+# copyArray.pop() # remove the last element of the array  
+
+# copyArray.remove(15) # remove the first occurrence of 15 from the array
+
+# for i in range(0,len(copyArray)):
+#      print(copyArray[i] , end=" ")
+
+
+##SLICING ARRAY
+# abc=val[2:5] # create a new array with elements from index 2 to 4
+# abc=val[2:-3] # create a new array with elements from index 2 to the third last index
+# abc=val[::-1] # create a new array with elements in reverse order
+# for i in range(0,len(abc)):
+#      print(abc[i] , end=" ")
+
+
+##ARRAY OF USER INPUT
+# arr=array('i', []) # create an empty array of integers
+# n=int(input("Enter number of elements:"))
+# for i in range (0,n): 
+#     arr.append(int(input("Enter element:"))) # add elements to the array
+# for x in arr:
+#     print(x,end=" ")
+
+
+# arr=array('i',[12,23,45,234,134,235])
+# i=arr.index(45) # find the index of the first occurrence of 45
+# print(i)
+
+
+##ARRAY USING NUMPY
+from numpy import *
+# val=array([1,2,4.5,'a']) # create a numpy array
+# val=array([1,2,4],float) 
+
+# val=linspace(10,20,11) # create an array of 11 equally spaced values between 10 and 20
+# val=arange(10,20,2) # create an array of values from 10 to 20 with a step of 2 and 20 is not included
+# val=logspace(10,20,2) # create an array of 2 values between 10^10 and 10^20
+# val=zeros(10) # create an array of 10 zeros
+# val=ones(10) # create an array of 10 ones
+# val=full(10,5) # create an array of 10 elements with the value 5
+# for x in val:
+#     print(x,end=" ") # print each element of the array on the same line separated by a space
+
+# for x in val:
+#     print(x) # print each element of the array on a separate line
+
+
+## Dimensional array
+# zero=array(10) 
+# print(zero)
+
+# one=array([1,2,3,4,5])
+# print(one)
+
+# two=array([[1,2,3],[4,5,6],[7,8,9]])
+# print(two)
+
+three=array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(three)
--- Singly Linked List.py
+++ Singly Linked List.py
@@ -36,6 +36,34 @@ class SinglyLinkedlist:
          temp.next=self.head # set the next pointer of the new node to the current head of the linked list
          self.head=temp # set the head pointer to the new node
 
+    def insertInMid(self,value,x):
+         temp=Node(value) # create a new node with the given value and next pointer set to None
+         t1=self.head # t1 is a temporary pointer to traverse the linked list
+         while(t1.next!=None): # traverse the linked list until a node with data part equal to x is found
+             if(t1.data==x): # if a node with data part equal to x is found, set the next pointer of the new node to the next pointer of the current node and set the next pointer of the current node to the new node
+                 temp.next=t1.next
+                 t1.next=temp
+             t1=t1.next # move the temporary pointer to the next node
+
+    def deleteLL(self,value):
+         t1=self.head # t1 is a temporary pointer to traverse the linked list
+         prev=t1 # prev is a pointer to keep track of the previous node while traversing the linked list
+         if(t1.data==value): # if the head node has data part equal to value, set the head pointer to the next node and delete the head node
+              self.head=t1.next
+         else:
+            while(t1.next!=None): # traverse the linked list until a node with data part equal to value is found
+                if(t1.data==value): # if a node with data part equal to value is found, set the next pointer of the previous node to the next pointer of the current node and delete the current node
+                    prev.next=t1.next
+                    break # exit the loop after deleting the node
+                else:
+                    prev=t1 # move the previous pointer to the current node
+                    t1=t1.next # move the temporary pointer to the next node
+            if(t1.data==value): # if the last node has data part equal to value, set the next pointer of the previous node to None and delete the last node
+                prev.next=None
+              
+
+             
+             
     def printLL(self): # method to print the linked list
         t1=self.head # t1 is a temporary pointer to traverse the linked list
         
@@ -55,6 +83,10 @@ obj.insertAtEnd(20) # add a new node with value 20 at the end of the linked list
 obj.insertAtEnd(30) # add a new node with value 30 at the end of the linked list
 
 obj.insertAtBeg(5) # add a new node with value 5 at the beginning of the linked list
+
+obj.insertInMid(40,20) # add a new node with value 15 after the node with value 20
+
+obj.deleteLL(30) # delete the node with value 30 from the linked list
 
 obj.printLL() # print the linked list 
 
