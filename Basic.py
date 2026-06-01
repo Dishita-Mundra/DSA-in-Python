eb4d3835db55dc5a354976a8b445d62d1de31d81
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-01 21:03:37 +0530

    DSA

--- Basic.py
+++ Basic.py
@@ -1,16 +1,50 @@
-print("Hello World")
+# print("Hello World")
 
 # this program finds the 
 # average attendance of 
 # monday and tuesday
 
-"""this program finds the 
+""" this program finds the 
 average attendance of 
-monday and tuesday"""
+monday and tuesday """
 
-monday=87    # attendance on monday
-tuesday=64   # attendance on tuesday
+# monday=87    # attendance on monday
+# tuesday=64   # attendance on tuesday
+# avg = (monday+tuesday)/2
+# print(avg)
 
-avg = (monday+tuesday)/2
+# print(34+324)
+# print(34-324)
+# print(34*324)
+# print(34/3)
+# print(34//3)
+# print(34%3)
+# print(2**3)
 
-print(avg)
+# a=5
+# a+=2   # a=a+2 -> a=7
+# a*=3   # a=a*3 -> a=21
+# print(a)
+
+# age=100
+# height=12.43
+# c=2+3j
+# s1="Hello World"
+# isGraduated=False
+# name=None
+
+# print(age,type(age))
+# print(height,type(height))
+# print(c,type(c))
+# print(s1,type(s1))
+# print(isGraduated,type(isGraduated))
+# print(name,type(name)) 
+
+# first_name=input("First Name:")
+# print("Hello",first_name)
+
+# age=int(input("Age:"))
+# print("Age is",age)
+
+# height=float(input("Height:"))
+# print("Height is",height)
