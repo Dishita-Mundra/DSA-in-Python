3c8087a088705da78389e0f8afec1dbcd440f0c1
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-01 20:29:32 +0530

    DSA

---
+++ Basic.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+# this program finds the 
+# average attendance of 
+# monday and tuesday
+
+"""this program finds the 
+average attendance of 
+monday and tuesday"""
+
+monday=87    # attendance on monday
+tuesday=64   # attendance on tuesday
+
+avg = (monday+tuesday)/2
+
+print(avg)
