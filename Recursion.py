11b3ed27452f2cdc97e72e2dcba1979238a900b6
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-02 22:43:36 +0530

    DSA

--- Recursion.py
+++ Recursion.py
@@ -5,6 +5,8 @@
 #     i+=1
 
 
+# TIME COMPLEXITY(O(n))
+
 # def printNumbers(i,n):
 #     # base case
 #     if i>n:
@@ -16,4 +18,38 @@
 
 
 
+# FACTORIAL (O(n))
+# def fact(n):
+#     if n==0:
+#         return 1
+#     return n*fact(n-1)
+# print(fact(4))
 
+
+
+# def fun(n):
+#     if n==0:
+#         return
+#     print(n,end=" ")
+#     fun(n-1)
+# fun(3)                    # OUTPUT:3 2 1
+
+
+# def fun(n):
+#     if n==0:
+#         return
+#     fun(n-1)
+#     print(n,end=" ")
+# fun(3)
+
+
+def gcd(a,b):
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+def lcm(a,b):
+    return a*b//gcd(a,b)
+
+print(gcd(15,50))
+print(lcm(15,50))
