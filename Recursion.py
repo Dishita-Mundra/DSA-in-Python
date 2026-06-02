25c3b0e7c2031bb36e51f74b0c3693d8415eb86d
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-02 21:57:55 +0530

    DSA

---
+++ Recursion.py
@@ -0,0 +1,19 @@
+# n = 5
+# i = 1
+# while i<=n:
+#     print(i,end=" ")
+#     i+=1
+
+
+# def printNumbers(i,n):
+#     # base case
+#     if i>n:
+#         return
+#     # recursive case
+#     print(i,end=" ")
+#     printNumbers(i+1,n)
+# printNumbers(1,5)
+
+
+
+
