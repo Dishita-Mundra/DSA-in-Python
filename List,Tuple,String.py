f9c17e34e74cedc3c08e8ea2b3b3cfe048b90fd9
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-03 16:55:27 +0530

    DSA

--- List,Tuple,String.py
+++ List,Tuple,String.py
@@ -1,11 +1,45 @@
-names = ["dishi","swati","aarti","anu"]
+# names = ["dishi","swati","aarti","anu"]
 
-print(names,type(names))
-print(type(names))
-print(len(names))
+# print(names,type(names))
+# print(type(names))
+# print(len(names))
 
-names[0] = "dishita"
+# names[0] = "dishita"
 
-print(names[0])
-print(names[2])
-print(names[-1])
+# print(names[0])
+# print(names[2])
+# print(names[-1])
+
+# list1=[5,"hello",34.23,True,214]
+# print(list1)
+
+# list1=[4,2,1,7,8,4]
+# list1.append(9)
+# list1.append([1,2,3,])
+# list1.extend([1,2,3,])
+
+# print(list1)
+
+
+# list1=[32,6,2,42,35,12]
+# list1.insert(3,100)
+
+# print(list1) 
+
+
+# list1=[32,6,2,42,35,12]
+# list1.remove(42)    # remove a given element
+# list1.pop()    # by default remove last element
+# list1.pop(1)   # remove element at index 1
+
+# print(list1)
+
+# list1.clear()
+# print(list1)
+
+
+# list1=[32,6,2,42,35,12]
+# print(min(list1))  # O(n)
+# print(max(list1))  # O(n)
+
+
