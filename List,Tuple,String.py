ea19083b036e93042668080e602c6798db0014c5
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-03 17:08:08 +0530

    DSA

--- List,Tuple,String.py
+++ List,Tuple,String.py
@@ -10,10 +10,10 @@
 # print(names[2])
 # print(names[-1])
 
-# list1=[5,"hello",34.23,True,214]
+# list1 = [5,"hello",34.23,True,214]
 # print(list1)
 
-# list1=[4,2,1,7,8,4]
+# list1 = [4,2,1,7,8,4]
 # list1.append(9)
 # list1.append([1,2,3,])
 # list1.extend([1,2,3,])
@@ -21,13 +21,13 @@
 # print(list1)
 
 
-# list1=[32,6,2,42,35,12]
+# list1 = [32,6,2,42,35,12]
 # list1.insert(3,100)
 
 # print(list1) 
 
 
-# list1=[32,6,2,42,35,12]
+# list1 = [32,6,2,42,35,12]
 # list1.remove(42)    # remove a given element
 # list1.pop()    # by default remove last element
 # list1.pop(1)   # remove element at index 1
@@ -38,8 +38,48 @@
 # print(list1)
 
 
-# list1=[32,6,2,42,35,12]
+# list1 = [32,6,2,42,35,12]
 # print(min(list1))  # O(n)
 # print(max(list1))  # O(n)
 
 
+# list1 = [4,1,2,5,3,2,1,3,2,4,5,3,2,1,2]
+
+# print(list1.count(1))
+# print(list1.count(2))
+# print(list1.count(3))
+# print(list1.count(4))
+# print(list1.count(5))
+
+# list1.sort()   # O(nlogn)
+# print(list1)
+
+# list1.reverse()   # O(n)
+# print(list1)
+
+
+# list1 = [1,5,3,2,5,7,3]
+
+# print(list1.index(2))
+# print(list1.index(5))
+# print(list1.index(5,2))
+
+
+
+# l1 = [1,2,3,]
+# l2 = l1       # DEEP COPY
+
+# l2.append(4)
+
+# print(l1)
+# print(l2)
+
+
+
+# l1 = [1,2,3,]
+# l2 = l1.copy()     # SHALLOW COPY
+
+# l2.append(4)
+
+# print(l1 , id(l1))
+# print(l2 , id(l2))
