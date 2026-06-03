7d81385a6670dc11e348b9bc4909dcd8b8959bc3
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-03 16:07:23 +0530

    DSA

---
+++ List,Tuple,String.py
@@ -0,0 +1,11 @@
+names = ["dishi","swati","aarti","anu"]
+
+print(names,type(names))
+print(type(names))
+print(len(names))
+
+names[0] = "dishita"
+
+print(names[0])
+print(names[2])
+print(names[-1])
