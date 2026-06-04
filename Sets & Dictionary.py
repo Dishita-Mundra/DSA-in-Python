52a95e6befa1c1077a7cbac710b99e13e9b7e055
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-04 13:22:23 +0530

    DSA

--- Sets & Dictionary.py
+++ Sets & Dictionary.py
@@ -1,3 +1,5 @@
+### SETS
+
 # set1 = {2,4,"hello",2,"dishita","hello",2,4}
 
 # print(set1)
@@ -60,24 +62,67 @@
 
 
 
-set1 = {1,8,5,2,4}
-set2 = {8,2,9,6}
+# set1 = {1,8,5,2,4}
+# set2 = {8,2,9,6}
 
 # union
-print(set1|set2)
-print(set1.union(set2))
+# print(set1|set2)
+# print(set1.union(set2))
 
 # intersection
-print(set1&set2)
-print(set1.intersection(set2))
+# print(set1&set2)
+# print(set1.intersection(set2))
 
 # difference
-print(set1-set2)
-print(set1.difference(set2))
+# print(set1-set2)
+# print(set1.difference(set2))
 
-print(set2-set1)
-print(set2.difference(set1))
+# print(set2-set1)
+# print(set2.difference(set1))
 
 # symmetric difference = union-intersection
-print(set1^set2)
-print(set1.symmetric_difference(set2))
+# print(set1^set2)
+# print(set1.symmetric_difference(set2))
+
+
+
+
+
+### DICTIONARIES
+
+# x = {}
+
+# print(type(x))
+
+
+# d1 = {1:"dishita" , "hello":100 , 10.21:"hii" , "key":True}
+
+# print(d1)
+# print(type(d1))
+# print(len(d1))
+
+# d1 = {[1,2,3]:"dishita" , "hello":100 , 10.21:"hii" , "key":True}  # Error as list is mutable
+# print(d1)
+
+# d1 = {(1,2,3):"dishita" , "hello":100 , 10.21:"hii" , "key":True}  # Error as list is mutable
+# print(d1)
+
+
+# d1 = {1:"dishita" , "hello":100 , 10.21:"hii" , "key":True}
+
+# print(dict[1])
+# print(dict["hello"])
+# print(d1)
+
+
+# d1 = {1:"dishita" , 2:"swati" , 3:"ananya" , 4:"anu"}
+
+# d1[5] = "swati"
+# d1[2] = "ani"
+
+# d1.update({1 :"dishi" , 10:"gungun"})
+
+# print(d1)
+
+d1=dict(dishita=1 , swati=2)
+print(d1)
