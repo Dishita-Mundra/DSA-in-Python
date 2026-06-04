41e41b0018fd099f4953f5eced82f9353ac1d13c
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-04 11:30:29 +0530

    DSA

---
+++ Sets & Dictionary.py
@@ -0,0 +1,59 @@
+# set1 = {2,4,"hello",2,"dishita","hello",2,4}
+
+# print(set1)
+# print(type(set1))
+# print(len(set1))
+
+
+
+# l1=[3,4,6,3,2,4,56,7,5,2,2,4,6,23,2,45,5]
+
+# count number of unique elements in this list
+# set1 = set(l1)
+# print(len(set1))
+# print(set1)
+
+
+
+# set1 = {'hello',2,4,'dishita'}
+
+# set1.add(100)
+# set1.add("hello")
+
+# set1.discard("hello")
+# set1.discard("hii")
+
+# set1.remove(2)
+# set1.remove("hii")   # ERROR
+
+# print(set1)
+
+
+
+# set1 = {'hello',2,4,'dishita'}
+
+# if "hello" in set1:
+#     print(True)
+# else:
+#     print(False)
+
+# if "hii" in set1:
+#     print(True)
+# else:
+#     print(False)
+
+
+# set1.clear()
+# print(set1)
+
+
+
+# set1 = {1,2,3}
+# set2 = set1  # DEEP COPY
+
+# set2 = set1.copy()  # SHALLOW COPY
+
+# set2.add(4)
+
+# print(set1)
+# print(set2)
