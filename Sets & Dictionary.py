f89e37425b1a76bf7b3b630fbb85dfaab5ce8897
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-04 12:56:18 +0530

    DSA

--- Sets & Dictionary.py
+++ Sets & Dictionary.py
@@ -57,3 +57,27 @@
 
 # print(set1)
 # print(set2)
+
+
+
+set1 = {1,8,5,2,4}
+set2 = {8,2,9,6}
+
+# union
+print(set1|set2)
+print(set1.union(set2))
+
+# intersection
+print(set1&set2)
+print(set1.intersection(set2))
+
+# difference
+print(set1-set2)
+print(set1.difference(set2))
+
+print(set2-set1)
+print(set2.difference(set1))
+
+# symmetric difference = union-intersection
+print(set1^set2)
+print(set1.symmetric_difference(set2))
