1e9d2411de49e25d54548cdd5db6e9d1cd23c2ac
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-07 00:29:37 +0530

    DSA

--- Sorting.py
+++ Sorting.py
@@ -69,52 +69,88 @@
 
 # Merge Sort
 
-def merge(nums, l, mid, r):
-    a = []
-    b = []
+# def merge(nums, l, mid, r):
+#     a = []
+#     b = []
 
-    for i in range(l, mid+1):
-        a.append(nums[i])
-    for i in range(mid+1, r+1):
-        b.append(nums[i])
+#     for i in range(l, mid+1):
+#         a.append(nums[i])
+#     for i in range(mid+1, r+1):
+#         b.append(nums[i])
 
-    i, j, k = 0, 0, l
+#     i, j, k = 0, 0, l
 
-    while k <= r:
-        if j == len(b):
-            nums[k] = a[i]
-            i += 1
-            k += 1
-        elif i == len(a):
-            nums[k] = b[j]
-            j += 1
-            k += 1
-        elif a[i] < b[j]:
-            nums[k] = a[i]
-            i += 1
-            k += 1
-        else:
-            nums[k] = b[j]
-            j += 1
-            k += 1
+#     while k <= r:
+#         if j == len(b):
+#             nums[k] = a[i]
+#             i += 1
+#             k += 1
+#         elif i == len(a):
+#             nums[k] = b[j]
+#             j += 1
+#             k += 1
+#         elif a[i] < b[j]:
+#             nums[k] = a[i]
+#             i += 1
+#             k += 1
+#         else:
+#             nums[k] = b[j]
+#             j += 1
+#             k += 1
 
 
-def mergeSort(nums, l, r):
+# def mergeSort(nums, l, r):
 
+#     # base case
+#     if l >= r:
+#         return
+    
+#     # recursive case
+#     mid = (l + r) // 2
+#     # l to mid
+#     mergeSort(nums, l, mid)
+#     mergeSort(nums, mid+1, r)
+
+#     merge(nums, l, mid, r)
+
+# def sortArray(nums):
+#     mergeSort(nums, 0, len(nums)-1)
+#     return nums
+
+# print(sortArray([3, 2, 8, 1, 7, 6, 4]))
+
+
+
+
+# Quick Sort
+
+def partition(nums, l, r):
+    key = nums[r]
+    start = l
+
+    for i in range(l, r+1):
+        if nums[i] >= key:
+            temp = nums[i]
+            nums[i] = nums[start]
+            nums[start] = temp
+            start += 1
+
+    return start-1
+
+def quickSort(nums, l, r):
     # base case
     if l >= r:
         return
     
-    # recursive case
-    mid = (l + r) // 2
-    # l to mid
-    mergeSort(nums, l, mid)
-    mergeSort(nums, mid+1, r)
+    p = partition(nums, l, r)
 
-    merge(nums, l, mid, r)
-
+    quickSort(nums, l, p-1)
+    quickSort(nums, p+1, r)
+    
 def sortArray(nums):
-    mergeSort(nums, 0, len(nums)-1)
+    n = len(nums)
+    quickSort(nums, 0, n-1)
+
     return nums
 
 print(sortArray([3, 2, 8, 1, 7, 6, 4]))
