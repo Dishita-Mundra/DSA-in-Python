79a8d263be8cba5287775dd8e409992c680cd46b
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-06 23:52:34 +0530

    JS

--- Sorting.py
+++ Sorting.py
@@ -45,21 +45,76 @@
 
 # Selection Sort
 
+# def sortArray(nums):
+#     n = len(nums)
+
+#     for i in range(n):
+#         mn = nums[i]
+#         ind = i
+#         for j in range(i+1, n):
+#             if nums[j] < mn:
+#                 mn = nums[j]
+#                 ind = j
+
+#         temp = nums[i]
+#         nums[i] = nums[ind]
+#         nums[ind] = temp
+
+#     return nums
+
+# print(sortArray([3, 2, 8, 1, 7, 6, 4]))
+
+
+
+
+# Merge Sort
+
+def merge(nums, l, mid, r):
+    a = []
+    b = []
+
+    for i in range(l, mid+1):
+        a.append(nums[i])
+    for i in range(mid+1, r+1):
+        b.append(nums[i])
+
+    i, j, k = 0, 0, l
+
+    while k <= r:
+        if j == len(b):
+            nums[k] = a[i]
+            i += 1
+            k += 1
+        elif i == len(a):
+            nums[k] = b[j]
+            j += 1
+            k += 1
+        elif a[i] < b[j]:
+            nums[k] = a[i]
+            i += 1
+            k += 1
+        else:
+            nums[k] = b[j]
+            j += 1
+            k += 1
+
+
+def mergeSort(nums, l, r):
+
+    # base case
+    if l>=r:
+        return
+    
+    # recursive case
+    mid = (l + r)//2
+    # l to mid
+    mergeSort(nums, l, mid)
+    mergeSort(nums, mid+1, r)
+
+    merge(nums, l, mid, r)
+
 def sortArray(nums):
-    n = len(nums)
-
-    for i in range(n):
-        mn = nums[i]
-        ind = i
-        for j in range(i+1, n):
-            if nums[j] < mn:
-                mn = nums[j]
-                ind = j
-
-        temp = nums[i]
-        nums[i] = nums[ind]
-        nums[ind] = temp
-
+    mergeSort(nums, 0, len(nums)-1)
     return nums
 
 print(sortArray([3, 2, 8, 1, 7, 6, 4]))
