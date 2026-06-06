5c7b7329174e2ee9ae2d80af7cd0e3e14c7f21bd
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-06 09:08:02 +0530

    DSA

--- Sorting.py
+++ Sorting.py
@@ -1,21 +1,41 @@
 # Bubble Sort
 
-def sortArray(nums):
-    n = len(nums)
+# def sortArray(nums):
+#     n = len(nums)
 
-    for i in range(n):
-        isSwap = False
-        for j in range(n-i-1):
-            if nums[j] > nums[j+1]:
-                #swap
-                temp = nums[j]
-                nums[j] = nums[j+1]
-                nums[j+1] = temp
-                isSwap = True
+#     for i in range(n):
+#         isSwap = False
+#         for j in range(n-i-1):
+#             if nums[j] > nums[j+1]:
+#                 #swap
+#                 temp = nums[j]
+#                 nums[j] = nums[j+1]
+#                 nums[j+1] = temp
+#                 isSwap = True
 
-        if not isSwap:
-            break
+#         if not isSwap:
+#             break
 
-    return nums
+#     return nums
     
-print(sortArray([1,3,6,5,3,2,235,45,567,456,345234,123,234,3453456]))
+# print(sortArray([3, 2, 8, 1, 7, 6, 4]))
+
+
+
+
+# Insertion Sort
+
+# def sortArray(nums):
+#     n = len(nums)
+
+#     for i in range(1,n):
+#         key = nums[i]
+#         j = i-1
+#         while j >= 0 and nums[j] > key:
+#             nums[j+1] = nums[j]
+#             j -= 1
+#         nums[j+1] = key
+        
+#     return nums
+
+# print(sortArray([3, 2, 8, 1, 7, 6, 4]))
