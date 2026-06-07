e972516f89d8d1d7ecfbdc9a140ac84d4513f1fc
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-07 18:55:30 +0530

    DSA

--- Searching.py
+++ Searching.py
@@ -1,31 +1,31 @@
 # Linear Search
 
-class Solution:
-    def search(nums, target):
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+# class Solution:
+#     def search(nums, target):
+#         for i in range(len(nums)):
+#             if nums[i] == target:
+#                 return i
         
-        return -1
+#         return -1
     
 
 
 
 # Binary Search
 
-def search(nums, target):
-        n = len(nums)
+# def search(nums, target):
+#         n = len(nums)
 
-        l = 0
-        r = n - 1
+#         l = 0
+#         r = n - 1
 
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-            elif target > nums[mid]:
-                l = mid + 1
-            else:
-                r = mid -1
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if target == nums[mid]:
+#                 return mid
+#             elif target > nums[mid]:
+#                 l = mid + 1
+#             else:
+#                 r = mid -1
 
-        return -1
+#         return -1
