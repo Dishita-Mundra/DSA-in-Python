57b8f1f8a7006ca7c2820cedfafaac5c40750448
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-07 18:54:41 +0530

    DSA

--- Searching.py
+++ Searching.py
@@ -8,3 +8,24 @@ class Solution:
         
         return -1
     
+
+
+
+# Binary Search
+
+def search(nums):
+        n = len(nums)
+
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid -1
+
+        return -1
